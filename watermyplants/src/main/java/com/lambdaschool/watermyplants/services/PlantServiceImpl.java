21d977c831209a1584ced04b38f62b341aca5626
Julliann Dulay <jullianndulay@gmail.com>
2020-09-19 10:55:59 -0700

    made interface for methods needed for plant controller

--- watermyplants/src/main/java/com/lambdaschool/watermyplants/services/PlantServiceImpl.java
+++ watermyplants/src/main/java/com/lambdaschool/watermyplants/services/PlantServiceImpl.java
@@ -1,10 +1,65 @@
 package com.lambdaschool.watermyplants.services;
 
+import com.lambdaschool.watermyplants.models.Plant;
+import com.lambdaschool.watermyplants.repository.PlantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 @Service(value = "plantService")
 public class PlantServiceImpl implements PlantService{
+
+    @Autowired
+    private PlantRepository plantrepos;
+
+    @Autowired
+    private HelperFunctions helperFunctions;
+
+    @Override
+    public List<Plant> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Plant> findByNameContaining(String nickname) {
+        return null;
+    }
+
+    @Override
+    public List<Plant> findBySpecies(String species) {
+        return null;
+    }
+
+    @Override
+    public List<Plant> findBySpeciesContaining(String species) {
+        return null;
+    }
+
+    @Override
+    public Plant findByPlantId(long id) {
+        return null;
+    }
+
+    @Override
+    public Plant findByName(String nickname) {
+        return null;
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+
+    @Override
+    public Plant save(Plant plant) {
+        return null;
+    }
+
+    @Override
+    public Plant update(Plant plant, long id) {
+        return null;
+    }
 }
