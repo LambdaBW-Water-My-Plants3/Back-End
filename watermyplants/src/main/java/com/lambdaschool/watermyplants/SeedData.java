f495dc6b7d54fb9aa94c92c736241b5e2f0331ec
Julliann Dulay <jullianndulay@gmail.com>
2020-09-20 07:37:57 -0700

    succesful update on deployment

--- watermyplants/src/main/java/com/lambdaschool/watermyplants/SeedData.java
+++ watermyplants/src/main/java/com/lambdaschool/watermyplants/SeedData.java
@@ -78,7 +78,6 @@ public class SeedData
                                    "admin@mymail.local"));
 
 
-
         // data, user
         User u2 = new User("test user",
                            "testuser1234",
